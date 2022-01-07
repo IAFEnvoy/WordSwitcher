3b8ff8e82d5389c25d55ee62447da38fc65be4d1
IAFEnvoy <tanksheng545@126.com>
2022-01-07 13:02:41 +0800

    添加项目文件。

---
+++ WordSwitcher/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WordSwitcher
+{
+    public partial class Form1 : Form
+    {
+        string datapath = Application.StartupPath + @"\data.txt";
+        CheckBox[,] data = new CheckBox[20, 20];
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 0; j <= 10; j++)
+                {
+                    data[i, j] = new CheckBox()
+                    {
+                        Location = new Point((i - 1) * 50 + 10, j * 20 + 5),
+                        Size = new Size(50, 20),
+                        Text = j.ToString()
+                    };
+                    this.Controls.Add(data[i, j]);
+                }
+            }
+            this.Width = 5 * 50 + 20;
+            this.Height = 10 * 25 + 20;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(datapath))
+            {
+                StreamReader sr = new StreamReader(datapath);
+                string line;
+                for (int i = 1; i <= 5; i++)
+                {
+                    line = sr.ReadLine();
+                    while (line == null)
+                        line = sr.ReadLine();
+                    string[] time = line.Split(' ');
+                    foreach (string s in time)
+                        if (!string.IsNullOrEmpty(s))
+                            data[i, int.Parse(s)].Checked = true;
+                }
+                sr.Close();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(datapath);
+            for (int i = 1; i <= 5; i++)
+            {
+                string line = "";
+                for (int j = 0; j <= 10; j++)
+                    if (data[i, j].Checked)
+                        line += j.ToString() + " ";
+                sw.WriteLine(line);
+            }
+            sw.Close();
+        }
+    }
+}
---
+++ WordSwitcher/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WordSwitcher
+{
+    internal static class Program
+    {
+        static DateTime[] start = new DateTime[20];
+        static List<string> list = new List<string>();
+        static string word = @"C:\Program Files\Microsoft Office\Office15\WINWORD.EXE";
+        static string wps = @"C:\Users\Administrator\AppData\Local\Kingsoft\WPS Office\11.1.0.11194\office6\wps.exe";
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            else
+            {
+                start[0] = new DateTime(2000, 1, 1, 7, 15, 0);
+                start[1] = new DateTime(2000, 1, 1, 8, 0, 0);
+                start[2] = new DateTime(2000, 1, 1, 9, 10, 0);
+                start[3] = new DateTime(2000, 1, 1, 10, 0, 0);
+                start[4] = new DateTime(2000, 1, 1, 10, 50, 0);
+                start[5] = new DateTime(2000, 1, 1, 11, 30, 0);
+                start[6] = new DateTime(2000, 1, 1, 13, 15, 0);
+                start[7] = new DateTime(2000, 1, 1, 14, 5, 0);
+                start[8] = new DateTime(2000, 1, 1, 15, 0, 0);
+                start[9] = new DateTime(2000, 1, 1, 15, 50, 0);
+                start[10] = new DateTime(2000, 1, 1, 16, 40, 0);
+
+                StreamReader sr = new StreamReader(@"C:\data.txt");
+                string line;
+                for (int i = 1; i <= 5; i++)
+                {
+                    line = sr.ReadLine();
+                    while (line == null)
+                        line = sr.ReadLine();
+                    string[] time = line.Split(' ');
+                    foreach (string s in time)
+                        if(!string.IsNullOrEmpty(s))
+                            list.Add(i.ToString() + "-" + s);
+                }
+
+                DateTime now = DateTime.Now;
+                DateTime today = new DateTime(2000, 1, 1, now.Hour, now.Minute, now.Second);
+                for (int i = 0; i <= 10; i++)
+                    if (today >= start[i].AddMinutes(-5) && today <= start[i].AddMinutes(45))//前后各5分钟缓冲
+                    {
+                        int weekday;
+                        switch (now.DayOfWeek)
+                        {
+                            case DayOfWeek.Sunday: { return; }
+                            case DayOfWeek.Monday: { weekday = 1; break; }
+                            case DayOfWeek.Tuesday: { weekday = 2; break; }
+                            case DayOfWeek.Wednesday: { weekday = 3; break; }
+                            case DayOfWeek.Thursday: { weekday = 4; break; }
+                            case DayOfWeek.Friday: { weekday = 5; break; }
+                            case DayOfWeek.Saturday: { return; }
+                            default: { return; }
+                        }
+                        string key = weekday.ToString() + "-" + i.ToString();
+                        if (list.Contains(key))
+                        {
+                            Process process1 = new Process();
+                            process1.StartInfo = new ProcessStartInfo(word, "\"" + args[0] + "\"");
+                            process1.Start();
+                            return;
+                        }
+                    }
+                    else
+                        break;
+                Process process = new Process();
+                process.StartInfo = new ProcessStartInfo(wps, "\"" + args[0] + "\"");
+                process.Start();
+            }
+        }
+    }
+}
