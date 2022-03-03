4b5ee315e4002e06d055d36eddd4aeaaa7e311d3
= <=>
2022-03-03 14:52:10 +0800

    修复一个神奇的bug

--- WordSwitcher/Program.cs
+++ WordSwitcher/Program.cs
@@ -11,7 +11,7 @@ namespace WordSwitcher
         static DateTime[] start = new DateTime[20];
         static List<string> list = new List<string>();
         static string word = @"C:\Program Files\Microsoft Office\Office15\WINWORD.EXE";
-        static string wps = @"C:\Users\Administrator\AppData\Local\Kingsoft\WPS Office\11.1.0.11194\office6\wps.exe";
+        static string wps = @"C:\Users\Administrator\AppData\Local\Kingsoft\WPS Office\ksolaunch.exe";
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
@@ -26,62 +26,67 @@ namespace WordSwitcher
             }
             else
             {
-                start[0] = new DateTime(2000, 1, 1, 7, 15, 0);
-                start[1] = new DateTime(2000, 1, 1, 8, 0, 0);
-                start[2] = new DateTime(2000, 1, 1, 9, 10, 0);
-                start[3] = new DateTime(2000, 1, 1, 10, 0, 0);
-                start[4] = new DateTime(2000, 1, 1, 10, 50, 0);
-                start[5] = new DateTime(2000, 1, 1, 11, 30, 0);
-                start[6] = new DateTime(2000, 1, 1, 13, 15, 0);
-                start[7] = new DateTime(2000, 1, 1, 14, 5, 0);
-                start[8] = new DateTime(2000, 1, 1, 15, 0, 0);
-                start[9] = new DateTime(2000, 1, 1, 15, 50, 0);
-                start[10] = new DateTime(2000, 1, 1, 16, 40, 0);
-
-                StreamReader sr = new StreamReader(@"C:\data.txt");
-                string line;
-                for (int i = 1; i <= 5; i++)
+                try
                 {
-                    line = sr.ReadLine();
-                    while (line == null)
-                        line = sr.ReadLine();
-                    string[] time = line.Split(' ');
-                    foreach (string s in time)
-                        if(!string.IsNullOrEmpty(s))
-                            list.Add(i.ToString() + "-" + s);
-                }
+                    start[0] = new DateTime(2000, 1, 1, 7, 15, 0);
+                    start[1] = new DateTime(2000, 1, 1, 8, 0, 0);
+                    start[2] = new DateTime(2000, 1, 1, 9, 10, 0);
+                    start[3] = new DateTime(2000, 1, 1, 10, 0, 0);
+                    start[4] = new DateTime(2000, 1, 1, 10, 50, 0);
+                    start[5] = new DateTime(2000, 1, 1, 11, 30, 0);
+                    start[6] = new DateTime(2000, 1, 1, 13, 15, 0);
+                    start[7] = new DateTime(2000, 1, 1, 14, 5, 0);
+                    start[8] = new DateTime(2000, 1, 1, 15, 0, 0);
+                    start[9] = new DateTime(2000, 1, 1, 15, 50, 0);
+                    start[10] = new DateTime(2000, 1, 1, 16, 40, 0);
 
-                DateTime now = DateTime.Now;
-                DateTime today = new DateTime(2000, 1, 1, now.Hour, now.Minute, now.Second);
-                for (int i = 0; i <= 10; i++)
-                    if (today >= start[i].AddMinutes(-5) && today <= start[i].AddMinutes(45))//前后各5分钟缓冲
+                    StreamReader sr = new StreamReader(Application.StartupPath + @"\data.txt");
+                    string line;
+                    for (int i = 1; i <= 5; i++)
                     {
-                        int weekday;
-                        switch (now.DayOfWeek)
-                        {
-                            case DayOfWeek.Sunday: { return; }
-                            case DayOfWeek.Monday: { weekday = 1; break; }
-                            case DayOfWeek.Tuesday: { weekday = 2; break; }
-                            case DayOfWeek.Wednesday: { weekday = 3; break; }
-                            case DayOfWeek.Thursday: { weekday = 4; break; }
-                            case DayOfWeek.Friday: { weekday = 5; break; }
-                            case DayOfWeek.Saturday: { return; }
-                            default: { return; }
-                        }
-                        string key = weekday.ToString() + "-" + i.ToString();
-                        if (list.Contains(key))
-                        {
-                            Process process1 = new Process();
-                            process1.StartInfo = new ProcessStartInfo(word, "\"" + args[0] + "\"");
-                            process1.Start();
-                            return;
-                        }
+                        line = sr.ReadLine();
+                        while (line == null)
+                            line = sr.ReadLine();
+                        string[] time = line.Split(' ');
+                        foreach (string s in time)
+                            if (!string.IsNullOrEmpty(s))
+                                list.Add(i.ToString() + "-" + s);
                     }
-                    else
-                        break;
-                Process process = new Process();
-                process.StartInfo = new ProcessStartInfo(wps, "\"" + args[0] + "\"");
-                process.Start();
+
+                    DateTime now = DateTime.Now;
+                    DateTime today = new DateTime(2000, 1, 1, now.Hour, now.Minute, now.Second);
+                    for (int i = 0; i <= 10; i++)
+                        if (today >= start[i].AddMinutes(-10) && today <= start[i].AddMinutes(40))//提前10分钟
+                        {
+                            int weekday;
+                            switch (now.DayOfWeek)
+                            {
+                                case DayOfWeek.Sunday: { return; }
+                                case DayOfWeek.Monday: { weekday = 1; break; }
+                                case DayOfWeek.Tuesday: { weekday = 2; break; }
+                                case DayOfWeek.Wednesday: { weekday = 3; break; }
+                                case DayOfWeek.Thursday: { weekday = 4; break; }
+                                case DayOfWeek.Friday: { weekday = 5; break; }
+                                case DayOfWeek.Saturday: { return; }
+                                default: { return; }
+                            }
+                            string key = weekday.ToString() + "-" + i.ToString();
+                            if (list.Contains(key))
+                            {
+                                Process process1 = new Process();
+                                process1.StartInfo = new ProcessStartInfo(word, "\"" + args[0] + "\"");
+                                process1.Start();
+                                return;
+                            }
+                        }
+                    Process process = new Process();
+                    process.StartInfo = new ProcessStartInfo(wps, "\"" + args[0] + "\"");
+                    process.Start();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.StackTrace);
+                }
             }
         }
     }
